b6d1bd0794c6af9599913d8e2f1723641bdb4097
Tinsaye Simeneh <64516393+tinsaye-simeneh@users.noreply.github.com>
2023-12-14 22:16:21 +0300

    Delete src directory

--- src/stores/bet/cancelAction.js
+++
@@ -1,7 +0,0 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import { Betaxios } from "../../services/api/axios";
-
-export const cancelAction = createAsyncThunk("Bet/PlaceBet", async (id) => {
-  const response = await Betaxios.post("cancel/" + id);
-  return response.data;
-});
