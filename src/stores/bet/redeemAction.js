0863dd23ca0c667f739287cff5f889da4a121d4a
Tinsaye Simeneh <64516393+tinsaye-simeneh@users.noreply.github.com>
2023-12-29 22:52:13 +0300

    Delete src directory

--- src/stores/bet/redeemAction.js
+++
@@ -1,7 +0,0 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import { Betaxios } from "../../services/api/axios";
-
-export const redeemAction = createAsyncThunk("Bet/redeemBet", async (id) => {
-  const response = await Betaxios.post("redeem/" + id);
-  return response.data;
-});
